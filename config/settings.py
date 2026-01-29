1c3248e7e7ab93e831a93d5e834b726b745b6d82
Manoranjith <164288436+Manoranjith1010@users.noreply.github.com>
2026-01-29 12:03:20 +0000

    Fix Django setup and app configuration

--- config/settings.py
+++ config/settings.py
@@ -42,6 +42,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "apps.accounts",
+    "apps.courses",
+    "apps.enrollments",
 ]
 
 MIDDLEWARE = [
